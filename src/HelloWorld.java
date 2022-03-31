b6922ac367dea4ef4d23e0a2a6ba784bb71e935b
omernecip <omernecip@gmail.com>
2022-03-31 09:23:55 +0300

    initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class HelloWorld {
+
+    //Doing some work here (you can type anything you want)
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("How are you ?");
+        String words= scan.next();
+        if(words.equals("fine")){
+            System.out.println("Hava a nice day");
+        }
+    }
+
 }
